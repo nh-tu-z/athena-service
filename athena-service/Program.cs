c7adc9cb9af0b4a53271412e382bca5e4ca485c9
Tu Ngo <nhtu1611@gmail.com>
2022-07-28 11:45:53 +0700

    init project

---
+++ athena-service/Program.cs
@@ -0,0 +1,22 @@
+using Serilog;
+
+namespace AthenaService
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
