44112a40af1b2f20b3e5bef1b2a3f0b0a51b9a4f
Tu Ngo <nhtu1611@gmail.com>
2022-08-02 22:02:16 +0700

    initing the background service on timer...

--- athena-service/CollectorCommunication/Scheduler/SchedulerService.cs
+++ athena-service/CollectorCommunication/Scheduler/SchedulerService.cs
@@ -9,9 +9,25 @@ namespace AthenaService.CollectorCommunication.Scheduler
     /// </summary>
     public class SchedulerService : IHostedService, IDisposable
     {
+        private readonly IServiceProvider _services;
+        private readonly ILogManager _logManager;
+        private readonly int _intervalInSeconds;
+
+        private Timer _timer = null;
+
+        SchedulerService(IServiceProvider services, IConfiguration configuration)
+        {
+            _services = services;
+
+            _logManager = services.GetRequiredService<ILogManager>();
+        }
+
         public async Task StartAsync(CancellationToken cancellationToken)
         {
+            _logManager.Information("Scheduler Service Has Started", GetType().Name);
 
+            var interval = _intervalInSeconds >= 300 ? _intervalInSeconds / 5 : _intervalInSeconds;
+            //_timer = new Timer()
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
