f1452361b7bb081cccb01d868ea23ff7638b4506
Tu Ngo <nhtu1611@gmail.com>
2022-08-08 10:50:22 +0700

    adding function for integration service

--- athena-service/Common/CommandText/CommandIntegrationText.cs
+++ athena-service/Common/CommandText/CommandIntegrationText.cs
@@ -3,5 +3,60 @@
     public class CommandIntegrationText
     {
         public const string GetById = @"SELECT * FROM Integration WHERE IntegrationId = @id";
-    }
+
+		public const string GetAll = @"
+			DECLARE @FirstRec INT, @LastRec INT
+			SET @FirstRec = @PageNo * @PageSize + 1;
+			SET @LastRec = (@PageNo + 1) * @PageSize;
+ 
+			; WITH CTE_Results AS  
+			(  
+			SELECT ROW_NUMBER() OVER (ORDER BY 
+			CASE WHEN (@SortColumn = 1 AND @SortDirection = 'asc')  
+				THEN IntegrationName  
+			END ASC,  
+			CASE WHEN (@SortColumn = 1 AND @SortDirection = 'desc')  
+				THEN IntegrationName
+			END DESC, 
+			CASE WHEN (@SortColumn = 2 AND @SortDirection = 'asc')  
+				THEN i.[Description]  
+			END ASC,  
+			CASE WHEN (@SortColumn = 2 AND @SortDirection = 'desc')  
+				THEN i.[Description]  
+			END DESC,
+			CASE WHEN (@SortColumn = 3 AND @SortDirection = 'asc')  
+				THEN env.[Description]  
+			END ASC,  
+			CASE WHEN (@SortColumn = 3 AND @SortDirection = 'desc')  
+				THEN env.[Description]  
+			END DESC,
+			CASE WHEN (@SortColumn = 4 AND @SortDirection = 'asc')  
+				THEN stt.[Description]
+			END ASC,  
+			CASE WHEN (@SortColumn = 4 AND @SortDirection = 'desc')  
+				THEN stt.[Description]  
+			END DESC
+			) AS RowNum,
+			COUNT(*) OVER() AS FilteredCount,
+			i.IntegrationId,
+			i.IntegrationName,
+			i.Environment,
+			i.[Description],
+			i.[State]
+			FROM Integration i
+			JOIN EnumDetail stt ON stt.[Value] = i.[State] AND stt.EnumTypeId = 1 -- EnumTypeId = 1 is IntegrationState 
+			JOIN EnumDetail env ON env.[Value] = i.Environment AND env.EnumTypeId = 2 -- EnumTypeId = 2 is EnvironmentType 
+			) SELECT
+				rs.IntegrationId, 
+				rs.IntegrationName, 
+				rs.Environment,
+				rs.[Description],
+				rs.[State],
+				FilteredCount
+			FROM CTE_Results rs
+			WHERE RowNum BETWEEN @FirstRec AND @LastRec
+			ORDER BY RowNum";
+
+		public const string GetAllIntegrationName = @"SELECT DISTINCT i.[IntegrationName] FROM Integration i";
+	}
 }
--- athena-service/Services/IntegrationService.cs
+++ athena-service/Services/IntegrationService.cs
@@ -1,7 +1,12 @@
-﻿using AthenaService.Interfaces;
+﻿using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using AthenaService.Interfaces;
 using AthenaService.Domain.Settings.Entities;
 using AthenaService.Persistence;
 using AthenaService.Common.CommandText;
+using AthenaService.Domain.Base;
 
 namespace AthenaService.Services
 {
@@ -16,5 +21,52 @@ namespace AthenaService.Services
 
         public async Task<Integration> GetByIdAsync(Guid id) =>
             await _persistenceService.QuerySingleOrDefaultAsync<Integration>(CommandIntegrationText.GetById, new { id });
+
+        public async Task<string> GenerateIntegrationTokenAsync(Guid integrationId, int tenantId)
+        {
+            var integration = await GetByIdAsync(integrationId);
+            if (integration == null)
+            {
+                // throw exception
+            }
+
+            // update tokenId in Integration
+            var secretKey = Guid.NewGuid();
+            var tokenId = Guid.NewGuid();
+            var token = GenerateJwtToken(secretKey.ToString(), new TimeSpan(36500), "athena", "athena", integrationId, tenantId, tokenId);
+            integration.TokenId = tokenId;
+            await _persistenceService.UpdateAsync(integration);
+
+            return token;
+        }
+
+        private static string GenerateJwtToken(string secretKey, TimeSpan? lifetime, string iss, string aud, Guid integrationId, int tenantId, Guid tokenId)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+            var claims = new List<Claim>
+            {
+                new Claim(CommonConstants.Claims.Id, integrationId.ToString(), ClaimValueTypes.String, integrationId.ToString()),
+                new Claim(CommonConstants.Claims.TenantId, tenantId.ToString(), ClaimValueTypes.String, integrationId.ToString()),
+                new Claim(CommonConstants.Claims.Jti, tokenId.ToString(), ClaimValueTypes.String, integrationId.ToString())
+            };
+
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+            JwtSecurityToken token = new(
+                    iss,
+                    aud,
+                    claims,
+                    DateTime.UtcNow,
+                    lifetime.HasValue ? DateTime.UtcNow + lifetime : DateTime.MaxValue,
+                    signingCredentials);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        public async Task<IList<string>> GetIntegrationNameAsync()
+        {
+            var listIntegrationNames = await _persistenceService.QueryAsync<string>(CommandIntegrationText.GetAllIntegrationName);
+            return listIntegrationNames.ToList();
+        }
     }
 }
