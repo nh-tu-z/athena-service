2b3c41c6dc145da3cf8f4c5fb6d25ab6d6d5882a
Tu Ngo <nhtu1611@gmail.com>
2022-08-09 10:52:21 +0700

    init tenant database schema service

---
+++ athena-service/Interfaces/ITenantDatabaseSchemaService.cs
@@ -0,0 +1,13 @@
+﻿using AthenaService.Domain.Models;
+using static AthenaService.Domain.Base.Enums;
+
+namespace AthenaService.Interfaces
+{
+    public interface ITenantDatabaseSchemaService
+    {
+        public Task<TenantDatabaseSchemaModel?> GetAsync(int tenantDbSchemaId);
+        public Task<TenantDatabaseSchemaModel?> GetAsync(int tenantId, string databaseName);
+        public Task<TenantDatabaseSchemaModel> CreateAsync(int tenantId, string databaseName, string schemaName);
+        public Task<TenantDatabaseSchemaModel> UpdateStateAsync(int tenantDatabaseSchemaId, TenantDatabaseSchemaState state);
+    }
+}
