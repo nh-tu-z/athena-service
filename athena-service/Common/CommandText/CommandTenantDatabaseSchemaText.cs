0d8ee4a8f118057cb5afecf4abfcf7eff6ba0dbc
Tu Ngo <nhtu1611@gmail.com>
2022-08-09 11:45:15 +0700

    implement tenant database schema service

---
+++ athena-service/Common/CommandText/CommandTenantDatabaseSchemaText.cs
@@ -0,0 +1,29 @@
+﻿namespace AthenaService.Common.CommandText
+{
+    public static class CommandTenantDatabaseSchemaText
+    {
+		private const string GET_TENANT_DB_SCHEMA_SELECT_FROM = @"SELECT s.[TenantDatabaseSchemaId], s.[DatabaseName], s.[SchemaName], s.[State]
+				, t.[TenantId], t.[TenantAlias], t.[Name], t.[State]
+				FROM [TenantDatabaseSchema] s
+				JOIN [Tenant] t on t.TenantId = s.TenantId
+				";
+
+		public static string GetTenantDatabaseSchema
+			=> GET_TENANT_DB_SCHEMA_SELECT_FROM +
+				"WHERE s.[TenantDatabaseSchemaId] = @TenantDatabaseSchemaId;";
+
+		public static string GetTenantDatabaseSchemaByTenantIdAndDatabaseName
+			=> GET_TENANT_DB_SCHEMA_SELECT_FROM +
+				"WHERE t.[TenantId] = @TenantId AND s.[DatabaseName] = @DatabaseName;";
+
+		public static string InsertTenantDatabaseSchema
+			=> @"INSERT INTO TenantDatabaseSchema ([TenantId], [DatabaseName], [SchemaName], [State]) 
+				OUTPUT INSERTED.TenantDatabaseSchemaId 
+				VALUES (@TenantId, @DatabaseName, @SchemaName, @State)";
+
+		public static string UpdateTenantDatabaseSchemaState
+			=> @"UPDATE TenantDatabaseSchema SET [State] = @State
+				OUTPUT INSERTED.TenantDatabaseSchemaId
+				WHERE TenantDatabaseSchemaId = @TenantDatabaseSchemaId";
+	}
+}
--- athena-service/Services/TenantDatabaseSchemaService.cs
+++ athena-service/Services/TenantDatabaseSchemaService.cs
@@ -1,29 +1,88 @@
-﻿using AthenaService.Interfaces;
+﻿using Dapper;
+using AthenaService.Interfaces;
+using AthenaService.Persistence;
 using AthenaService.Domain.Models;
+using AthenaService.Common.CommandText;
 using static AthenaService.Domain.Base.Enums;
 
 namespace AthenaService.Services
 {
     public class TenantDatabaseSchemaService : ITenantDatabaseSchemaService
     {
-        public Task<TenantDatabaseSchemaModel?> GetAsync(int tenantDbSchemaId)
-        {
+        private readonly IPersistenceService _adminPersistenceService;
+        private readonly ITenantService _tenantService;
 
+        public TenantDatabaseSchemaService(IPersistenceService persistenceService, ITenantService tenantService)
+        {
+            _adminPersistenceService = persistenceService ?? throw new ArgumentNullException(nameof(persistenceService));
+            _tenantService = tenantService ?? throw new ArgumentNullException(nameof(tenantService));
         }
 
-        public Task<TenantDatabaseSchemaModel?> GetAsync(int tenantId, string databaseName)
+        private async Task<TenantDatabaseSchemaModel?> GetAsync(
+                string query, object parameters)
         {
-
+            var tenantDatabaseSchemas = await _adminPersistenceService
+                .QueryAsync<TenantDatabaseSchemaModel, TenantModel, TenantDatabaseSchemaModel>(
+                query, (tenantDbSchema, tenant) =>
+                {
+                    tenantDbSchema.Tenant = tenant;
+                    return tenantDbSchema;
+                }, parameters, splitOn: nameof(TenantModel.TenantId));
+            return tenantDatabaseSchemas.FirstOrDefault();
         }
 
-        public Task<TenantDatabaseSchemaModel> CreateAsync(int tenantId, string databaseName, string schemaName)
+        public async Task<TenantDatabaseSchemaModel?> GetAsync(int tenantDbSchemaId)
         {
-
+            var parameters = new { TenantDatabaseSchemaId = tenantDbSchemaId };
+            string query = CommandTenantDatabaseSchemaText.GetTenantDatabaseSchema;
+            return await GetAsync(query, parameters);
         }
 
-        public Task<TenantDatabaseSchemaModel> UpdateStateAsync(int tenantDatabaseSchemaId, TenantDatabaseSchemaState state)
+        public async Task<TenantDatabaseSchemaModel?> GetAsync(int tenantId, string databaseName)
         {
+            var parameters = new { TenantId = tenantId, DatabaseName = databaseName };
+            string query = CommandTenantDatabaseSchemaText.GetTenantDatabaseSchemaByTenantIdAndDatabaseName;
+            return await GetAsync(query, parameters);
+        }
 
+        public async Task<TenantDatabaseSchemaModel> CreateAsync(int tenantId, string databaseName, string schemaName)
+        {
+            var tenant = await _tenantService.GetTenantAsync(tenantId);
+            if (tenant == null)
+            {
+                // throw 
+            }
+
+            var parameters = new DynamicParameters();
+            parameters.Add("TenantId", tenantId);
+            parameters.Add("DatabaseName", databaseName);
+            parameters.Add("SchemaName", schemaName);
+            parameters.Add("State", (int)TenantDatabaseSchemaState.NotReady);
+
+            var tenantDbSchemaId = await _adminPersistenceService.QuerySingleOrDefaultAsync<int>(
+                CommandTenantDatabaseSchemaText.InsertTenantDatabaseSchema, parameters);
+            TenantDatabaseSchemaModel tenantDatabaseSchema = new();
+            tenantDatabaseSchema.TenantDatabaseSchemaId = tenantDbSchemaId;
+            tenantDatabaseSchema.DatabaseName = databaseName;
+            tenantDatabaseSchema.SchemaName = schemaName;
+            tenantDatabaseSchema.State = TenantDatabaseSchemaState.NotReady;
+            tenantDatabaseSchema.TenantId = tenantId;
+            return tenantDatabaseSchema;
+        }
+
+        public async Task<TenantDatabaseSchemaModel> UpdateStateAsync(int tenantDatabaseSchemaId, TenantDatabaseSchemaState state)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("TenantDatabaseSchemaId", tenantDatabaseSchemaId);
+            parameters.Add("State", (int)state);
+            var tenantDbSchemaId = await _adminPersistenceService.QuerySingleOrDefaultAsync<int>(
+                CommandTenantDatabaseSchemaText.UpdateTenantDatabaseSchemaState, parameters);
+            TenantDatabaseSchemaModel? tenantDatabaseSchema = await GetAsync(tenantDbSchemaId);
+            if (tenantDatabaseSchema == null)
+            {
+                // throw 
+            }
+            return tenantDatabaseSchema;
         }
     }
 }
